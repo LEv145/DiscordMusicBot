805d9011cc682a1c654d771303693fb99c895e88
LEv145 <levxxx03@gmail.com>
2022-01-17 20:05:13 +0200

    Update `database/database.py`

--- database/database.py
+++ database/database.py
@@ -6,15 +6,17 @@ from sqlalchemy.orm import sessionmaker, scoped_session
 
 class Database():
     def __init__(self, url: str):
-        session_factory = sessionmaker(
-            bind=create_async_engine(url),
-            class_=AsyncSession,
-        )
-        self.session = scoped_session(session_factory())
-
-    async def init(self):
         start_mappers()
 
+        self.engine = create_async_engine(url)
+
+        base_session_factory = sessionmaker(
+            bind=self.engine,
+            class_=AsyncSession,
+        )
+
+        self.session: AsyncSession = base_session_factory()
+
     async def close(self):
-        await self.session.session_factory.close()
+        await self.session.close()
 
