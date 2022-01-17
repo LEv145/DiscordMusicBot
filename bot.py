56a316bc863403bb190d326170b1e70d32ce84d0
LEv145 <levxxx03@gmail.com>
2022-01-17 23:59:50 +0200

    Add typing

--- bot.py
+++ bot.py
@@ -6,17 +6,17 @@ from loguru import logger
 class Bot(commands.InteractionBot):
     """My BOt"""
 
-    def __init__(self):
+    def __init__(self) -> None:
         super().__init__(
             intents=disnake.Intents.all()
         )
         self.update_description.start()
 
-    async def on_ready(self):
+    async def on_ready(self) -> None:
         logger.info(f"We have logged in as {self.user}")
 
     @tasks.loop(seconds=10)
-    async def update_description(self):
+    async def update_description(self) -> None:
         if self.is_ready():
             activity = disnake.Activity(
                 name=f"{len(self.guilds)} servers", type=disnake.ActivityType.listening
