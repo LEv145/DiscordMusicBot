ca5e97861bb83c5f3a6644bd30fa77990039134a
LEv145 <levxxx03@gmail.com>
2022-01-18 18:44:17 +0200

    Transferred utils

---
+++ utils/__init__.py
@@ -0,0 +1,21 @@
+from .discord import (
+    DefaultEmbed,
+    InfoDefaultEmbed,
+)
+from .tools import (
+    DeltatimeTemplate,
+    bool_to_emoji,
+    format_optional_string,
+    shorten,
+    strdelta,
+)
+
+__all__ = [
+    "DefaultEmbed",
+    "DeltatimeTemplate",
+    "InfoDefaultEmbed",
+    "bool_to_emoji",
+    "format_optional_string",
+    "shorten",
+    "strdelta",
+]
