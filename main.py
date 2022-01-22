9791713dd70d50532db1e0bb32e7dbbde7396b52
LEv145 <levxxx03@gmail.com>
2022-01-22 17:28:05 +0200

    Delete ShardReadyEvent `listen`

--- main.py
+++ main.py
@@ -2,19 +2,15 @@ from pathlib import Path
 
 from config import BOT_TOKEN
 
-import hikari
 from lightbulb import BotApp
 from loguru import logger
 
 
 logger.add(Path("log/main.log"), rotation="500 MB")
 
-bot = BotApp(token=BOT_TOKEN)
+bot = BotApp(token=BOT_TOKEN, prefix="!")
 
-
-@bot.listen(hikari.ShardReadyEvent)
-async def on_ready(event: hikari.ShardReadyEvent) -> None:
-    logger.info(f"We have logged in as {event.my_user}")
+bot.load_extensions("extensions.commands.music")
 
 
 bot.run()
