6e751082fe90b2e4db07a117bdb62f2d170fc0f0
LEv145 <levxxx03@gmail.com>
2022-01-26 23:59:54 +0200

    Update extensions

---
+++ extensions/commands/music_extension.py
@@ -0,0 +1,29 @@
+from __future__ import annotations
+
+from typing import (
+    TYPE_CHECKING,
+)
+
+import lightbulb
+
+
+if TYPE_CHECKING:
+    from bot import BaseBot
+
+
+plugin = lightbulb.Plugin("Music")  # TODO: Rename
+
+
+@plugin.command()
+@lightbulb.command(name="ping", description="Ping!")
+@lightbulb.implements(lightbulb.SlashCommand)
+async def ping(ctx: lightbulb.Context) -> None:
+    await ctx.respond("Pong<3 ~~")
+
+
+def load(bot: BaseBot) -> None:
+    bot.add_plugin(plugin)
+
+
+def unload(bot: BaseBot) -> None:
+    bot.remove_plugin(plugin)
