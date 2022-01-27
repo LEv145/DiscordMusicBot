8a2add51238cadb5de0f277d9a9bc0bfe8005d9b
LEv145 <levxxx03@gmail.com>
2022-01-27 20:05:27 +0200

    Add banner

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import sys
 from pathlib import Path
 
 from config import BOT_TOKEN
@@ -13,11 +14,14 @@ EXTENSIONS = (
 
 logger.add(Path("log/main.log"), rotation="500 MB")
 
+
+sys.path.append('/static')
 bot = BaseBot(
     token=BOT_TOKEN,
     owner_ids=(501089151089770517,),
     intents=Intents.NONE,
     default_enabled_guilds=(867344761970229258,),
+    banner="hikari_musicbot_banner",
 )
 
 
