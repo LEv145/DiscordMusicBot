a0e0f1d13e4d4b828aed8dc09306712499ce0d8c
LEv145 <levxxx03@gmail.com>
2022-02-04 23:40:49 +0200

    Add `join` command

--- extensions/commands/music_extension.py
+++ extensions/commands/music_extension.py
@@ -12,6 +12,7 @@ from lyricstranslate import (
 )
 import lightbulb
 from lightbulb.ext import filament
+from songbird.hikari import Voicebox
 
 from utils.discord import DefaultEmbed
 
@@ -48,6 +49,43 @@ async def command_search_track(ctx: lightbulb.Context, query: str) -> None:
         await ctx.respond(
             DefaultEmbed(description="```" + "\n\n".join(track_result.lyrics) + "```"),
         )
+
+
+@plugin.command()
+@lightbulb.command("join", "Join to channel!")
+@lightbulb.implements(lightbulb.SlashCommand)
+async def command_join(ctx: lightbulb.Context) -> None:
+    states = ctx.bot.cache.get_voice_states_view_for_guild(ctx.guild_id)
+    voice_states = states.iterator().filter(lambda i: i.user_id == ctx.author.id)
+
+    try:
+        voice_state = await voice_states.__anext__()
+    except StopAsyncIteration:
+        # FIXME?: Unicode
+        await ctx.respond(DefaultEmbed(description="You're not on the music channel(´• ω •)"))
+        return
+
+    await Voicebox.connect(
+        client=ctx.bot,
+        guild_id=ctx.guild_id,
+        channel_id=voice_state.channel_id
+    )
+    await ctx.respond(DefaultEmbed(description="I'm connected(* ^ ω ^)"))
+
+
+# @plugin.command()
+# @lightbulb.option("query", "Query for play")
+# @lightbulb.command(name="play", description="Play music!")
+# @lightbulb.implements(lightbulb.SlashCommand)
+# @filament.utils.pass_options  # type: ignore
+# async def command_play(ctx: lightbulb.Context, query: str) -> None:
+#     voice = await Voicebox.connect(
+#         client=ctx.bot,
+#         guild_id=ctx.guild_id,
+#         channel_id=ctx.channel_id,
+#     )
+#     voice.play_source(Source)
+#     track_handle = await voice.play_source(Source.ffmpeg())
 
 
 def load(bot: BaseBot) -> None:
