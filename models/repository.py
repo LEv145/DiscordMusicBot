4e0e345d84f822536d57ff4ed7e6a04e6ab959d7
LEv145 <levxxx03@gmail.com>
2022-02-06 14:05:18 +0200

    Update naming (`__cache` -> `_cache`)

--- models/repository.py
+++ models/repository.py
@@ -13,17 +13,18 @@ if typing.TYPE_CHECKING:
 
 class DictVoiceRepository(ABCVoiceRepository):
     def __init__(self) -> None:
-        self.__cache: dict[int, Voicebox] = {}
+        self._cache: dict[int, Voicebox] = {}
 
     async def add(self, id_: int, voice: Voicebox) -> None:
         """Add voicebox."""
-        self.__cache[id_] = voice
+        if id_ not in self._cache:
+            self._cache[id_] = voice
 
     async def get(self, id_: int) -> Voicebox | None:
         """Get voicebox by id."""
-        return self.__cache.get(id_)
+        return self._cache.get(id_)
 
     async def remove(self, id_: int) -> None:
         """Remove voicebox by id."""
-        self.__cache.pop(id_, None)
+        self._cache.pop(id_, None)
 
